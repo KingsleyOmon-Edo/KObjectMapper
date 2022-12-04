3f3b8422a5c1c1fa35d4b41b653bc7ff4a42d265
KingsleyOmon-Edo <10457868+KingsleyOmon-Edo@users.noreply.github.com>
2022-12-04 17:18:33 -0500

    Refactorihg: Moved LINQ extension methods to the LinqExtensions type. No regressions.

---
+++ src/Extensions/LinqExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Extensions;
+using static Extensions.ObjectExtensions;
+
+namespace Extensions
+{
+    public static class LinqExtensions
+    {
+        public static IEnumerable<T> Except<T>(this IEnumerable<T> leftList, IEnumerable<T> rightList, Func<T, T, bool> predicate)
+        {
+            return leftList.Except(rightList, new LambdaComparer<T>(predicate));
+        }
+
+        public static IEnumerable<PropertyInfo> Except<PropertyInfo, TPropContainer>(this IEnumerable<PropertyInfo> leftList,
+                                                                                     IEnumerable<PropertyInfo> rightList,
+                                                                                     Func<TPropContainer, PropertyInfo, TPropContainer, PropertyInfo, bool> predicate,
+                                                                                     TPropContainer leftPropContainer,
+                                                                                     TPropContainer rightPropContainer)
+        {
+            return leftList.Except(rightList, new PropertyValueComparer<PropertyInfo, TPropContainer>(predicate, leftPropContainer, rightPropContainer));
+        }
+    }
+}
--- src/Extensions/ObjectExtensions.cs
+++ src/Extensions/ObjectExtensions.cs
@@ -7,20 +7,6 @@
 
     public static partial class ObjectExtensions
     {
-        public static IEnumerable<T> Except<T>(this IEnumerable<T> leftList, IEnumerable<T> rightList, Func<T, T, bool> predicate)
-        {
-            return leftList.Except(rightList, new LambdaComparer<T>(predicate));
-        }
-
-        public static IEnumerable<PropertyInfo> Except<PropertyInfo, TPropContainer>(this IEnumerable<PropertyInfo> leftList,
-                                                                                     IEnumerable<PropertyInfo> rightList,
-                                                                                     Func<TPropContainer, PropertyInfo, TPropContainer, PropertyInfo, bool> predicate,
-                                                                                     TPropContainer leftPropContainer,
-                                                                                     TPropContainer rightPropContainer)
-        {
-            return leftList.Except(rightList, new PropertyValueComparer<PropertyInfo, TPropContainer>(predicate, leftPropContainer, rightPropContainer));
-        }
-
         public static List<PropertyInfo> GetPropertyDiffs<T>(this T source, T target)
         {
             ValidateParameters(source, target);
