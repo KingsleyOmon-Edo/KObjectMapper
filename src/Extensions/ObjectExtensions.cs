b1a5d53bf9f5accc9a98aa44cac353aae5eb6310
KingsleyOmon-Edo <10457868+KingsleyOmon-Edo@users.noreply.github.com>
2022-12-04 17:01:18 -0500

    Refactoring: Move types to their own files, No regressions.

--- src/Extensions/ObjectExtensions.cs
+++ src/Extensions/ObjectExtensions.cs
@@ -1,90 +1,12 @@
 ﻿namespace Extensions
 {
     using System;
-    using System.Collections;
     using System.Collections.Generic;
-    using System.Diagnostics.CodeAnalysis;
     using System.Linq;
     using System.Reflection;
 
-    public static class ObjectExtensions
+    public static partial class ObjectExtensions
     {
-        public class StructuralEqualityComparer<T> : IEqualityComparer<T>
-        {
-            public bool Equals(T x, T y)
-            {
-                return StructuralComparisons.StructuralEqualityComparer.Equals(x, y);
-            }
-
-            public int GetHashCode([DisallowNull] T obj)
-            {
-                return StructuralComparisons.StructuralEqualityComparer.GetHashCode(obj);
-            }
-        }
-        public class PropertyInfoComparer : EqualityComparer<PropertyInfo>
-        {
-            public override bool Equals(PropertyInfo? x, PropertyInfo? y)
-            {
-                //  Check names - PropNameComparer
-                //  Check types - PropTypeComparer
-                //  Check values - PropValueComparer
-
-                return StructuralComparisons.StructuralEqualityComparer.Equals(x, y);
-            }
-
-            public override int GetHashCode([DisallowNull] PropertyInfo obj)
-            {
-                return StructuralComparisons.StructuralEqualityComparer.GetHashCode(obj);
-            }
-        }
-
-        public class LambdaComparer<T> : IEqualityComparer<T>
-        {
-            private readonly Func<T, T, bool> _predicate;
-
-            public LambdaComparer(Func<T, T, bool> predicate)
-            {
-                this._predicate = predicate;
-            }
-
-            public bool Equals(T? x, T? y)
-            {
-                return _predicate(x, y);
-            }
-
-            public int GetHashCode([DisallowNull] T obj)
-            {
-                return 0;
-            }
-        }
-
-        public class PropertyValueComparer<PropertyInfo, TPropContainer> : IEqualityComparer<PropertyInfo>
-        {
-            private readonly Func<TPropContainer, PropertyInfo, TPropContainer, PropertyInfo, bool> _predicate;
-            private readonly TPropContainer _leftPropContainer;
-            private readonly TPropContainer _rightPropContainer;
-
-            public PropertyValueComparer(Func<TPropContainer, PropertyInfo, TPropContainer, PropertyInfo, bool> predicate,
-                                         TPropContainer leftPropContainer,
-                                         TPropContainer rightPropContainer)
-            {
-                _predicate = predicate ?? throw new ArgumentNullException(nameof(predicate));
-                
-                this._leftPropContainer = leftPropContainer;
-                this._rightPropContainer = rightPropContainer;
-            }
-
-            public bool Equals(PropertyInfo? x, PropertyInfo? y)
-            {
-                return _predicate(_leftPropContainer, x, _rightPropContainer, y);             
-            }
-
-            public int GetHashCode([DisallowNull] PropertyInfo obj)
-            {
-                return obj.GetHashCode();
-            }
-        }
-
         public static IEnumerable<T> Except<T>(this IEnumerable<T> leftList, IEnumerable<T> rightList, Func<T, T, bool> predicate)
         {
             return leftList.Except(rightList, new LambdaComparer<T>(predicate));
@@ -190,8 +112,6 @@
             }
 
             return target;
-
-            // Simplicity is the ultimate sophistication.
         }
         public static object SendUpdatesTo(this object source, object target)
         {
