c76c0e7c63dae62ed2b8c7ad6d7f6335715a434b
KingsleyOmon-Edo <10457868+KingsleyOmon-Edo@users.noreply.github.com>
2022-12-31 16:17:23 -0500

    Add Property type check logic to checer class

--- src/ObjectMapper/Checker.cs
+++ src/ObjectMapper/Checker.cs
@@ -8,4 +8,12 @@ public class Checker
         target = target ?? throw new ArgumentNullException(nameof(target));
         return source;
     }
+
+    public static void PropertyTypeCheck<T>(T source, T target)
+    {
+        if (source.GetType() != target.GetType())
+        {
+            throw new ArgumentException($"{nameof(source)} and {nameof(target)} objects should be of the same type");
+        }
+    }
 }
