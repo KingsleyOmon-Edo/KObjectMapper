c46d9d57ab0002210b02f7764d63a99ec4405f36
KingsleyOmon-Edo <10457868+KingsleyOmon-Edo@users.noreply.github.com>
2022-12-31 16:03:06 -0500

    Refactoring: Moved null check logic to a newly introduced Checker class

---
+++ src/ObjectMapper/Checker.cs
@@ -0,0 +1,11 @@
+﻿namespace ObjectMapper;
+
+public class Checker
+{
+    public static T NullChecks<T>(T source, T target)
+    {
+        source = source ?? throw new ArgumentNullException(nameof(source));
+        target = target ?? throw new ArgumentNullException(nameof(target));
+        return source;
+    }
+}
