71efe0520a8ff650273957d166a82988ff4adbc0
KingsleyOmon-Edo <10457868+KingsleyOmon-Edo@users.noreply.github.com>
2022-12-31 12:41:46 -0500

    Cohesion: Keep the remaining MapFrom() test methods in the MapFromTests class

--- tests/ObjectMapperTests/MapFromTests.cs
+++ tests/ObjectMapperTests/MapFromTests.cs
@@ -21,7 +21,7 @@ public class MapFromTests
         testCustomer.FirstName.Should().Be(testEmployee.FirstName);
         testCustomer.LastName.Should().Be(testEmployee.LastName);
     }
-
+    
     [Fact]
     public void Non_generic_MapFrom_should_correctly_map_compatible_props_of_two_dissimilar_objects()
     {
@@ -39,78 +39,6 @@ public class MapFromTests
     }
 
     [Fact]
-    public void Mapping_with_MapFromOfT_via_type_inference_should_succeed()
-    {
-        var sut = Mapper.Create();
-        var sourceCustomer = ObjectMother.SampleCustomer;
-        var targetCustomer = new Customer
-        {
-            Id = 2_000,
-            FirstName = "Epicurus",
-            LastName = "Osemede",
-            PhoneNumber = "4027771115"
-        };
-
-        sut.MapFrom(sourceCustomer, targetCustomer);
-
-        _commonAsserts.AssertSimilarCustomers(sourceCustomer, targetCustomer);
-    }
-
-    [Fact]
-    public void Calling_MapFromOfT_on_a_satisfactory_object_without_mapper_should_succeed()
-    {
-        var sourceCustomer = ObjectMother.SampleCustomer;
-        var targetCustomer = new Customer
-        {
-            Id = 500,
-            FirstName = "Sean",
-            LastName = "Daniels",
-            PhoneNumber = "1234567890",
-        };
-
-        targetCustomer.MapFrom<Customer>(sourceCustomer);
-
-        _commonAsserts.AssertSimilarCustomers(targetCustomer, sourceCustomer);
-        _commonAsserts.AssertSimilarCustomers(sourceCustomer, targetCustomer);
-
-    }
-
-    [Fact]
-    public void Invoking_MapFromOfT_on_a_default_object_instance_should_work()
-    {
-        var sourceProduct = ObjectMother.SampleProduct;
-        var targetProduct = new Product();
-
-        targetProduct.MapFrom<Product>(sourceProduct);
-
-        _commonAsserts.AssertSimilarProducts(sourceProduct, targetProduct);
-        _commonAsserts.AssertSimilarProducts(targetProduct, sourceProduct);
-    }
-
-    [Fact]
-    public void Type_inference_should_work_for_MapFromOfT()
-    {
-        var sourceCustomer = ObjectMother.SampleCustomer;
-        var targetCustomer = new Customer();
-
-        targetCustomer.MapFrom(sourceCustomer);
-
-        _commonAsserts.AssertSimilarCustomers(sourceCustomer, targetCustomer);
-    }
-
-    [Fact]
-    public void Invoking_MapFromOfT_on_a_default_object_instance_should_succeed()
-    {
-        var sourceProduct = ObjectMother.SampleProduct;
-        var targetProduct = new Product();
-
-        targetProduct.MapFrom<Product>(sourceProduct);
-
-        _commonAsserts.AssertSimilarProducts(sourceProduct, targetProduct);
-        _commonAsserts.AssertSimilarProducts(targetProduct, sourceProduct);
-    }
-
-    [Fact]
     public void Mapping_with_the_MapFromOfT_overload_should_succeed()
     {
         //  Arrange
@@ -125,24 +53,21 @@ public class MapFromTests
         };
 
         //  Act         
-        sut.MapFrom<Customer>(sourceCustomer, targetCustomer);
+        sut.MapFrom(sourceCustomer, targetCustomer);
 
         //  Assert
         _commonAsserts.AssertSimilarCustomers(sourceCustomer, targetCustomer);
     }
-
+    
     [Fact]
     public void Passing_a_null_source_object_to_non_generic_MapFrom_throws_ArgumentNullException()
     {
         var sut = Mapper.Create();
 
         Product sourceProduct = null;
-        Product targetProduct = ObjectMother.SampleProduct;
+        var targetProduct = ObjectMother.SampleProduct;
 
-        Assert.Throws<ArgumentNullException>(() =>
-        {
-            sut.MapFrom(sourceProduct as object, targetProduct as object);
-        });
+        Assert.Throws<ArgumentNullException>(() => { sut.MapFrom(sourceProduct, targetProduct as object); });
     }
 
     [Fact]
@@ -153,9 +78,6 @@ public class MapFromTests
         var sourceCustomer = ObjectMother.SampleCustomer;
         Customer? targetCustomer = null;
 
-        Assert.Throws<ArgumentNullException>(() =>
-        {
-            sut.MapFrom(sourceCustomer as object, targetCustomer as object);
-        });
+        Assert.Throws<ArgumentNullException>(() => { sut.MapFrom(sourceCustomer, targetCustomer as object); });
     }
 }
