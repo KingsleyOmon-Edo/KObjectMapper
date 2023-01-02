838f17f8232da1938d0a9f93ca3844bd60781b21
KingsleyOmon-Edo <10457868+KingsleyOmon-Edo@users.noreply.github.com>
2023-01-01 19:48:22 -0500

    Code clean  up

--- tests/ObjectMapperTests/MapFromOfTTests.cs
+++ tests/ObjectMapperTests/MapFromOfTTests.cs
@@ -1,95 +1,85 @@
-﻿using FluentAssertions;
-using ObjectMapper;
+﻿using ObjectMapper;
 using ObjectMapper.Extensions;
 using ObjectMapperTests.Helpers;
 
-namespace ObjectMapperTests;
-
-public class MapFromOfTTests
+namespace ObjectMapperTests
 {
-    //  ToDo: Validate types of source objects
-    //  ToDo: Validate types of target objects
-    //  ToDo: Validate both types when different types are supplied for each Map<TSource, TTarget>(TSource source, TTarget target)
-    //  ToDo: Validate the type ignorance still works.
-    //  ToDo: POC: Setup a sample, install the nuget and test. Fix any issues.
-
-    private readonly CommonAsserts _commonAsserts;
-
-    public MapFromOfTTests()
+    public class MapFromOfTTests
     {
-        _commonAsserts = CommonAsserts.Create();
-    }
-    
-    [Fact]
-    public void Mapping_with_MapFromOfT_via_type_inference_should_succeed()
-    {
-        var sut = Mapper.Create();
-        var sourceCustomer = ObjectMother.SampleCustomer;
-        var targetCustomer = new Customer
+        private readonly CommonAsserts _commonAsserts;
+
+        public MapFromOfTTests() => _commonAsserts = CommonAsserts.Create();
+
+        [Fact]
+        public void Mapping_with_MapFromOfT_via_type_inference_should_succeed()
         {
-            Id = 2_000,
-            FirstName = "Epicurus",
-            LastName = "Osemede",
-            PhoneNumber = "4027771115"
-        };
+            var sut = Mapper.Create();
+            var sourceCustomer = ObjectMother.SampleCustomer;
+            var targetCustomer = new Customer
+            {
+                Id = 2_000,
+                FirstName = "Epicurus",
+                LastName = "Osemede",
+                PhoneNumber = "4027771115"
+            };
 
-        sut.MapFrom<Customer>(sourceCustomer, targetCustomer);
+            sut.MapFrom(sourceCustomer, targetCustomer);
 
-        _commonAsserts.AssertSimilarCustomers(sourceCustomer, targetCustomer);
-    }
+            _commonAsserts.AssertSimilarCustomers(sourceCustomer, targetCustomer);
+        }
 
-    [Fact]
-    public void Calling_MapFromOfT_on_a_satisfactory_object_without_mapper_should_succeed()
-    {
-        var sourceCustomer = ObjectMother.SampleCustomer;
-        var targetCustomer = new Customer
+        [Fact]
+        public void Calling_MapFromOfT_on_a_satisfactory_object_without_mapper_should_succeed()
         {
-            Id = 500,
-            FirstName = "Sean",
-            LastName = "Daniels",
-            PhoneNumber = "1234567890"
-        };
+            var sourceCustomer = ObjectMother.SampleCustomer;
+            var targetCustomer = new Customer
+            {
+                Id = 500,
+                FirstName = "Sean",
+                LastName = "Daniels",
+                PhoneNumber = "1234567890"
+            };
 
-        targetCustomer.MapFrom<Customer>(sourceCustomer);
+            targetCustomer.MapFrom(sourceCustomer);
 
-        _commonAsserts.AssertSimilarCustomers(targetCustomer, sourceCustomer);
-        _commonAsserts.AssertSimilarCustomers(sourceCustomer, targetCustomer);
+            _commonAsserts.AssertSimilarCustomers(targetCustomer, sourceCustomer);
+            _commonAsserts.AssertSimilarCustomers(sourceCustomer, targetCustomer);
+        }
+
+        [Fact]
+        public void Invoking_MapFromOfT_on_a_default_object_instance_should_work()
+        {
+            var sourceProduct = ObjectMother.SampleProduct;
+            var targetProduct = new Product();
+
+            targetProduct.MapFrom(sourceProduct);
+
+            _commonAsserts.AssertSimilarProducts(sourceProduct, targetProduct);
+            _commonAsserts.AssertSimilarProducts(targetProduct, sourceProduct);
+        }
+
+        [Fact]
+        public void Type_inference_should_work_for_MapFromOfT()
+        {
+            var sourceCustomer = ObjectMother.SampleCustomer;
+            var targetCustomer = new Customer();
+
+            //  Call MapFrom<T>() by type inference.
+            targetCustomer.MapFrom(sourceCustomer);
+
+            _commonAsserts.AssertSimilarCustomers(sourceCustomer, targetCustomer);
+        }
+
+        [Fact]
+        public void Invoking_MapFromOfT_on_a_default_object_instance_should_succeed()
+        {
+            var sourceProduct = ObjectMother.SampleProduct;
+            var targetProduct = new Product();
+
+            targetProduct.MapFrom(sourceProduct);
+
+            _commonAsserts.AssertSimilarProducts(sourceProduct, targetProduct);
+            _commonAsserts.AssertSimilarProducts(targetProduct, sourceProduct);
+        }
     }
-
-    [Fact]
-    public void Invoking_MapFromOfT_on_a_default_object_instance_should_work()
-    {
-        var sourceProduct = ObjectMother.SampleProduct;
-        var targetProduct = new Product();
-
-        targetProduct.MapFrom<Product>(sourceProduct);
-
-        _commonAsserts.AssertSimilarProducts(sourceProduct, targetProduct);
-        _commonAsserts.AssertSimilarProducts(targetProduct, sourceProduct);
-    }
-
-    [Fact]
-    public void Type_inference_should_work_for_MapFromOfT()
-    {
-        var sourceCustomer = ObjectMother.SampleCustomer;
-        var targetCustomer = new Customer();
-
-        //  Call MapFrom<T>() by type inference.
-        targetCustomer.MapFrom(sourceCustomer);
-
-        _commonAsserts.AssertSimilarCustomers(sourceCustomer, targetCustomer);
-    }
-
-    [Fact]
-    public void Invoking_MapFromOfT_on_a_default_object_instance_should_succeed()
-    {
-        var sourceProduct = ObjectMother.SampleProduct;
-        var targetProduct = new Product();
-
-        targetProduct.MapFrom<Product>(sourceProduct);
-
-        _commonAsserts.AssertSimilarProducts(sourceProduct, targetProduct);
-        _commonAsserts.AssertSimilarProducts(targetProduct, sourceProduct);
-    }
-
 }
--- tests/ObjectMapperTests/MapToIEnumerableOfTTests.cs
+++ tests/ObjectMapperTests/MapToIEnumerableOfTTests.cs
@@ -2,34 +2,29 @@
 using ObjectMapper;
 using ObjectMapperTests.Helpers;
 
-namespace ObjectMapperTests;
-
-public class MapToIEnumerableOfTTests
+namespace ObjectMapperTests
 {
-    //  ToDo: MapToEnIEnumerable: By TDD implement guard clauses for source collection.
-    //  Todo: MapToEnIEnumerable: By TDd implement guard clauses for target collection.
-    //  ToDo: MapToEnIEnumerable: By TDD validate that source and target objects are IEnumerable<T> derived instances.
-
-
-    [Fact]
-    public void Mapping_from_one_IEnumerableOfT_to_another_should_succeed()
+    public class MapToIEnumerableOfTTests
     {
-        var sut = Mapper.Create();
-        var sourceCollection = new List<Product>()
+        [Fact]
+        public void Mapping_from_one_IEnumerableOfT_to_another_should_succeed()
         {
-            new Product { Id = 0, Description = "Tennis Racket", Price = 25.00M, Quantity = 1 },
-            new Product { Id = 1, Description = "Golf Ball", Price = 10.00M, Quantity = 4 },
-            new Product { Id = 2, Description = "Laptop Computer", Price = 1_000.00M, Quantity = 2 }
-        };
+            var sut = Mapper.Create();
+            var sourceCollection = new List<Product>
+            {
+                new() { Id = 0, Description = "Tennis Racket", Price = 25.00M, Quantity = 1 },
+                new() { Id = 1, Description = "Golf Ball", Price = 10.00M, Quantity = 4 },
+                new() { Id = 2, Description = "Laptop Computer", Price = 1_000.00M, Quantity = 2 }
+            };
 
-        var targetCollection = new List<Product>();
+            var targetCollection = new List<Product>();
 
-        targetCollection = sut.MapFrom<Product>(sourceCollection).ToList();
+            targetCollection = sut.MapFrom(sourceCollection).ToList();
 
-        targetCollection.Count.Should().BeGreaterThan(0);
-        targetCollection.Count.Should().Be(sourceCollection.Count);
-        targetCollection[0].Id.Should().Be(sourceCollection[0].Id);
-        targetCollection.Should().BeOfType<List<Product>>();
-
+            targetCollection.Count.Should().BeGreaterThan(0);
+            targetCollection.Count.Should().Be(sourceCollection.Count);
+            targetCollection[0].Id.Should().Be(sourceCollection[0].Id);
+            targetCollection.Should().BeOfType<List<Product>>();
+        }
     }
 }
