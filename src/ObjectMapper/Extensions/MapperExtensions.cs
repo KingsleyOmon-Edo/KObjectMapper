967fcba3dea8e771acd8bdb3fbc103f3fbba07b1
KingsleyOmon-Edo <10457868+KingsleyOmon-Edo@users.noreply.github.com>
2023-01-03 10:14:25 -0500

    Reviewing the object Mapper object API

--- src/ObjectMapper/Extensions/MapperExtensions.cs
+++ src/ObjectMapper/Extensions/MapperExtensions.cs
@@ -4,25 +4,6 @@
 
     public static class MapperExtensions
     {
-        //public static void MapFrom<TCommon>(this TCommon source, TCommon target)
-        //{
-        //    source = source ?? throw new ArgumentNullException(nameof(source));
-        //    target = target ?? throw new ArgumentNullException(nameof(target));
-
-        //    var mapper = Mapper.Create();
-        //    mapper.Map(target, source);
-        //}
-
-        //public static void MapTo<TCommon>(this TCommon source, TCommon target)
-        //{
-        //    Checker.NullCheckAll<TCommon>();
-
-        //    var mapper = Mapper.Create();
-        //    mapper.Map(source, target);
-        //}
-
-        //  ====================================
-
         public static void MapTo(this object source, object target)
         {
             Checker.NullCheckAll(source, target);
--- src/ObjectMapper/Mapper.cs
+++ src/ObjectMapper/Mapper.cs
@@ -11,46 +11,6 @@
         {
         }
 
-
-        //public void Map<TCommon>(TCommon source, TCommon target)
-        //{
-        //    source = Checker.NullChecks(source, target);
-
-        //    MappingService.Create().ApplyDiffs(source, target);
-        //}
-
-        //public void MapFrom<TCommon>(TCommon source, TCommon target)
-        //{
-        //    Checker.NullChecks(source, target);
-
-        //    MappingService.Create().ApplyDiffs(target, source);
-        //}
-
-
-        //public void MapTo<TCommon>(TCommon source, TCommon target)
-        //{
-        //    Checker.NullChecks(source, target);
-
-        //    MappingService.Create().ApplyDiffs(source, target);
-        //}
-
-        //public IEnumerable<TCommon> MapFrom<TCommon>(IEnumerable<TCommon> source)
-        //    where TCommon : new()
-        //{
-        //    var resultCollection = new List<TCommon>();
-
-        //    foreach (var sourceElem in source)
-        //    {
-        //        var targetElem = new TCommon();
-        //        sourceElem.MapTo(targetElem);
-
-        //        resultCollection.Add(targetElem);
-        //    }
-
-        //    return resultCollection;
-        //}
-
-        //  ====================================
         public void MapTo(object source, object target)
         {
             Checker.NullChecks(source, target);
