15ac0c02b6c89a62c32de40c4417c97b49b6e336
KingsleyOmon-Edo <10457868+KingsleyOmon-Edo@users.noreply.github.com>
2023-01-04 09:07:02 -0500

    Implement the generics API with the following signature:
    -  Map<TSource, TTarget>(TSource source, TTarget target)
    -  MapTo<TTarget>(TTarget target)
    -  MapFrom<TSource>(TSource source)

--- src/ObjectMapper/Extensions/MapperExtensions.cs
+++ src/ObjectMapper/Extensions/MapperExtensions.cs
@@ -4,20 +4,22 @@
 
     public static class MapperExtensions
     {
-        public static void MapTo(this object source, object target)
-        {
-            Checker.NullCheckAll(source, target);
-
-            var mapper = new Mapper();
-            mapper.Map(source, target);
-        }
-
         public static void MapFrom(this object source, object target)
         {
             Checker.NullCheckAll(source, target);
 
             var mapper = new Mapper();
             mapper.MapFrom(source, target);
+        }
+
+        public static void MapTo<TTarget>(this object source, TTarget target)
+        {
+            //var mapper = Mapper.Create();
+            //mapper.Map(source, target);
+            //  Mapping.
+
+            var svc = MappingService.Create();
+            svc.ApplyDiffs(source, target);
         }
     }
 }
---
+++ tests/ObjectMapperTests/Abstractions/ITypedMappingOfTTests.cs
@@ -0,0 +1,8 @@
+﻿namespace ObjectMapperTests.Abstractions
+{
+    public interface ITypedMappingOfTTests
+    {
+        void Passing_a_source_object_incompatible_with_the_source_type_parameter_should_fail();
+        void Pass_a_target_object_incompatible_with_the_target_type_parameter_should_fail();
+    }
+}
