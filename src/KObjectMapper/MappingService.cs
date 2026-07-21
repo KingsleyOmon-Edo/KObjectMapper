6941c7754f3b2a154a235c8d7d11f0271852662f
Kingsley Omon-Edo <kingsley.omonedo@gmail.com>
2026-07-21 17:45:39 -0400

    refactor(mapping): convert MappingService to instance methods
    - Refactored MappingService so all methods except Create are now instance methods, enforcing instance-based usage and enabling future stateful extensions.
    - Updated all internal calls to use `this.` instead of static references.
    - Added MappingServiceTests with two unit tests for ApplyDiffs and GetPropertyDiffs, using Shouldly and ObjectMother for test data.
    
    Changes made by: Kingsley Omon-Edo

--- src/KObjectMapper/MappingService.cs
+++ src/KObjectMapper/MappingService.cs
@@ -14,22 +14,25 @@ public class MappingService
 
     public static MappingService Create() => new();
 
-    private static void ValidateParameters<T>(T source, T target)
+    private void ValidateParameters<T>(T source, T target)
     {
+        _ = this.GetHashCode();
         Checker.NullChecks(source, target);
         Checker.TypeChecks(source, target);
     }
 
-    private static void PropertyNullChecks<T>(T sourceObject, PropertyInfo sourceProp, T targetObject,
+    private void PropertyNullChecks<T>(T sourceObject, PropertyInfo sourceProp, T targetObject,
         PropertyInfo targetProp)
     {
+        _ = this.GetHashCode();
         _ = Checker.NullChecks(sourceObject, targetObject);
         _ = Checker.NullChecks(sourceProp, targetProp);
     }
 
-    private static void ComparePropertyTypes(PropertyInfo sourceProp, PropertyInfo targetProp,
+    private void ComparePropertyTypes(PropertyInfo sourceProp, PropertyInfo targetProp,
         out Type sourcePropType, out Type targetPropType)
     {
+        _ = this.GetHashCode();
         sourcePropType = sourceProp.PropertyType;
         targetPropType = targetProp.PropertyType;
         if (Equals(sourcePropType, targetPropType) == false)
@@ -39,19 +42,19 @@ public class MappingService
         }
     }
 
-    public static void ArePropValuesDifferent(object sourceObject, PropertyInfo sourceProp, object targetObject,
+    public void ArePropValuesDifferent(object sourceObject, PropertyInfo sourceProp, object targetObject,
         PropertyInfo targetProp)
     {
-        MappingService.ArePropValuesDifferent<object>(sourceObject, sourceProp, targetObject, targetProp);
+        this.ArePropValuesDifferent<object>(sourceObject, sourceProp, targetObject, targetProp);
     }
 
-    public static bool ArePropValuesDifferent<T>(T sourceObject, PropertyInfo sourceProp, T targetObject,
+    public bool ArePropValuesDifferent<T>(T sourceObject, PropertyInfo sourceProp, T targetObject,
         PropertyInfo targetProp)
     {
-        MappingService.PropertyNullChecks(sourceObject, sourceProp, targetObject, targetProp);
+        this.PropertyNullChecks(sourceObject, sourceProp, targetObject, targetProp);
         Checker.PropertyNameCheck(sourceProp, targetProp);
         Type sourcePropType, targetPropType;
-        MappingService.ComparePropertyTypes(sourceProp, targetProp, out sourcePropType, out targetPropType);
+        this.ComparePropertyTypes(sourceProp, targetProp, out sourcePropType, out targetPropType);
 
         object? sourcePropValue = sourceProp.GetValue(sourceObject);
         object? targetPropValue = targetProp.GetValue(targetObject);
@@ -67,24 +70,24 @@ public class MappingService
         return Equals(sourceValue, targetValue);
     }
 
-    public static List<PropertyInfo> GetPropertyDiffs<T>(T source, T target)
+    public List<PropertyInfo> GetPropertyDiffs<T>(T source, T target)
     {
         Checker.NullChecks(source, target);
         Checker.TypeChecks(source, target);
-        var diffs = MappingService.ComputeDiffs(source, target);
+        var diffs = this.ComputeDiffs(source, target);
 
         return diffs;
     }
 
-    public static List<PropertyInfo> GetPropertyDiffs(object source, object target)
+    public List<PropertyInfo> GetPropertyDiffs(object source, object target)
     {
         Checker.NullChecks(source, target);
-        var diffs = MappingService.ComputeDiffs(source, target);
+        var diffs = this.ComputeDiffs(source, target);
 
         return diffs;
     }
 
-    private static List<PropertyInfo> ComputeDiffs<T>(T source, T target)
+    private List<PropertyInfo> ComputeDiffs<T>(T source, T target)
     {
         ArgumentNullException.ThrowIfNull(source);
         ArgumentNullException.ThrowIfNull(target);
@@ -96,7 +99,7 @@ public class MappingService
                 object sourceObject,
                 PropertyInfo sourceProp,
                 object targetObject,
-                PropertyInfo targetProp) => MappingService.ArePropValuesDifferent<object>(sourceObject,
+                PropertyInfo targetProp) => this.ArePropValuesDifferent<object>(sourceObject,
                 sourceProp,
                 targetObject,
                 targetProp), source, target)
@@ -111,11 +114,11 @@ public class MappingService
         object safeSource = source;
         object safeTarget = target;
 
-        var diffs = MappingService.GetPropertyDiffs(safeSource, safeTarget);
+        var diffs = this.GetPropertyDiffs(safeSource, safeTarget);
         var sourceProps = safeSource.GetType().GetProperties();
 
         _ = sourceProps;
-        MappingService.WriteToProperties(safeSource, safeTarget, diffs);
+        this.WriteToProperties(safeSource, safeTarget, diffs);
 
         return safeTarget;
     }
@@ -128,18 +131,19 @@ public class MappingService
         T safeSource = source;
         T safeTarget = target;
 
-        MappingService.ValidateParameters(safeSource, safeTarget);
+        this.ValidateParameters(safeSource, safeTarget);
 
-        var diffs = MappingService.GetPropertyDiffs(safeSource, safeTarget);
+        var diffs = this.GetPropertyDiffs(safeSource, safeTarget);
         var sourceProps = safeSource.GetType().GetProperties();
         _ = sourceProps;
-        MappingService.WriteToProperties(safeSource, safeTarget, diffs);
+        this.WriteToProperties(safeSource, safeTarget, diffs);
 
         return safeTarget;
     }
 
-    private static void WriteToProperties<T>(T source, T target, List<PropertyInfo> diffs)
+    private void WriteToProperties<T>(T source, T target, List<PropertyInfo> diffs)
     {
+        _ = this.GetHashCode();
         ArgumentNullException.ThrowIfNull(source);
         ArgumentNullException.ThrowIfNull(target);
 
@@ -161,29 +165,29 @@ public class MappingService
         }
     }
 
-    public static object SendUpdatesTo(object source, object target) =>
-        MappingService.Create().ApplyDiffs(source, target);
+    public object SendUpdatesTo(object source, object target) =>
+        this.ApplyDiffs(source, target);
 
-    public static T Patch<T>(T source, T target) => MappingService.Create().ApplyDiffs(source, target);
+    public T Patch<T>(T source, T target) => this.ApplyDiffs(source, target);
 
-    public static T AcceptChanges<T>(T target, T source) =>
-        MappingService.Create().ApplyDiffs(source, target);
+    public T AcceptChanges<T>(T target, T source) =>
+        this.ApplyDiffs(source, target);
 
-    public static T SendChanges<T>(T source, T target) =>
-        MappingService.Create().ApplyDiffs(source, target);
+    public T SendChanges<T>(T source, T target) =>
+        this.ApplyDiffs(source, target);
 
-    public static T AcceptPatch<T>(T target, T source) =>
-        MappingService.Create().ApplyDiffs(source, target);
+    public T AcceptPatch<T>(T target, T source) =>
+        this.ApplyDiffs(source, target);
 
-    public static T SendPatches<T>(T source, T target) =>
-        MappingService.Create().ApplyDiffs(source, target);
+    public T SendPatches<T>(T source, T target) =>
+        this.ApplyDiffs(source, target);
 
-    public static T PatchFrom<T>(T target, T source) => MappingService.Create().ApplyDiffs(source, target);
-    public static T PatchTo<T>(T sources, T target) => MappingService.Create().ApplyDiffs(sources, target);
+    public T PatchFrom<T>(T target, T source) => this.ApplyDiffs(source, target);
+    public T PatchTo<T>(T sources, T target) => this.ApplyDiffs(sources, target);
 
-    public static T AcceptUpdates<T>(T target, T source) =>
-        MappingService.Create().ApplyDiffs(source, target);
+    public T AcceptUpdates<T>(T target, T source) =>
+        this.ApplyDiffs(source, target);
 
-    public static T SendUpdates<T>(T source, T target) =>
-        MappingService.Create().ApplyDiffs(source, target);
+    public T SendUpdates<T>(T source, T target) =>
+        this.ApplyDiffs(source, target);
 }
---
+++ tests/KObjectMapperTests/MappingServiceTests.cs
@@ -0,0 +1,45 @@
+using System.Reflection;
+using KObjectMapper;
+using KObjectMapperTests.Helpers;
+using Shouldly;
+
+namespace KObjectMapperTests;
+
+public class MappingServiceTests
+{
+    [Fact]
+    public void ApplyDiffs_DifferentPropertyValues_UpdatesTargetInstance()
+    {
+        MappingService mappingService = MappingService.Create();
+        Customer source = ObjectMother.SampleCustomer;
+        Customer target = new()
+        {
+            Id = source.Id,
+            FirstName = "Before",
+            LastName = source.LastName,
+            PhoneNumber = source.PhoneNumber
+        };
+
+        mappingService.ApplyDiffs(source, target);
+
+        target.FirstName.ShouldBe(source.FirstName);
+    }
+
+    [Fact]
+    public void GetPropertyDiffs_DifferentPropertyValues_ReturnsTheDifferentProperties()
+    {
+        MappingService mappingService = MappingService.Create();
+        Customer source = ObjectMother.SampleCustomer;
+        Customer target = new()
+        {
+            Id = source.Id,
+            FirstName = "Before",
+            LastName = source.LastName,
+            PhoneNumber = source.PhoneNumber
+        };
+
+        List<PropertyInfo> diffs = mappingService.GetPropertyDiffs(source, target);
+
+        diffs.Select(diff => diff.Name).ShouldContain(nameof(Customer.FirstName));
+    }
+}
